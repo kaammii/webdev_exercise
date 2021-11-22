7ddc8c220a990d668eacd5960134587635d90b4b
Kamran Nazir <kamrannazir692@gmail.com>
2021-11-22 14:39:42 +0500

    feat: create skills model

--- backend/models.py
+++ backend/models.py
@@ -5,10 +5,14 @@ from pydantic import BaseModel
 
 db = SQLAlchemy()
 
+user_skills = db.Table('user_skills', db.Column('user_id', db.Integer, db.ForeignKey(
+    'user.id')), db.Column('skill_id', db.Integer, db.ForeignKey('skill.id')))
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    skills = db.relationship('Skill', secondary=user_skills, backref=db.backref('users', lazy='subquery'))
 
     def __repr__(self):
         return "<User %r>" % self.name
@@ -17,10 +21,30 @@ class User(db.Model):
 class UserSchema(BaseModel):
     id: int
     name: str
+    skills: List[str]
+
+    class Config:
+        orm_mode = True
+
+class UsersResponse(BaseModel):
+    users: List[UserSchema]
+
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<Skill %r>" % self.name
+
+
+class SkillSchema(BaseModel):
+    id: int
+    name: str
 
     class Config:
         orm_mode = True
 
 
-class UsersResponse(BaseModel):
-    items: List[UserSchema]
+class SkillsResponse(BaseModel):
+    skills: List[SkillSchema]
